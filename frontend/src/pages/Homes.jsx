95ab4d8a275df9ec4a07d7e19272f80954eb63e9
Your Name <youremail@example.com>
2025-08-09 19:58:27 +0530

    Initial commit

---
+++ frontend/src/pages/Homes.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import HeroSection from '../components/HeroSection'
+import FeatureSection from '../components/FeatureSection'
+import TrailerSection from '../components/TrailerSection'
+
+const Homes = () => {
+  return (
+    <>
+      <HeroSection />
+      <FeatureSection />
+      <TrailerSection />
+    </>
+  )
+}
+
+export default Homes
+
