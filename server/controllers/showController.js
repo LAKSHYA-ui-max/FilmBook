49c70e671e7c52f1419c1538b982071d10b91138
Your Name <youremail@example.com>
2025-08-11 17:45:42 +0530

    model import fix

---
+++ server/controllers/showController.js
@@ -0,0 +1,125 @@
+import axios from "axios"
+import Movie from "../models/Movie.js";
+import Show from "../models/Show.js";
+
+//API to get now playing movies from TMDB API
+export const getNowPlayingMovies = async (req, res) =>{
+    try {
+        const {data} = await axios.get('', {
+            headers : {Authorization : `Bearer $`}
+        })
+
+        const movies = data.results;
+        res.json({success : true, movies : movies})
+    } catch (error) {
+        console.error(error);
+        res.json({success : false, message : error.message})
+    }
+    
+}
+
+//API to add a new show to the database
+export const addShow = async (req, res)=>{
+    try{
+        const {movieId, showInput, showPrice} =req.body
+
+        let movie = await Movie.findById(movieId)
+
+        if(!movie)
+        {
+            //Fetch movie details and credits from TMDB API
+            const[movieDetailsResponse, movieCreditsResponse] = await Promise.all([
+                axios.get(``,{            
+                    headers : {Authorization : `Bearer $`}
+                }),
+                axios.get(``, {
+                    headers : {Authorization : `Bearer $`}
+                })
+            ]);
+
+            const movieApiData = movieDetailsResponse.data;
+            const movieCreditsData = movieCreditsResponse.data;
+
+            const movieDetails = {
+                _id : movieId,
+                title : movieApiData.title,
+                overview : movieApiData.overview,
+                poster_path : movieApiData.poster_path,
+                backdrop_path : movieApiData.backdrop_path,
+                genres : movieApiData.genres,
+                casts : movieApiData.casts,
+                release_date : movieApiData.release_date,
+                original_language : movieApiData.original_language,
+                tagline : movieApiData.tagline || "",
+                vote_average : movieApiData.vote_average,
+                runtime : movieApiData.runtime,
+            }
+
+            // Add movie to the database
+            movie = await Movie.create(movieDetails)
+        }
+
+        const showsToCreate = [];
+        showInput.forEach(show => {
+            const showDate = show.data;
+            show.time.forEach((time)=>{
+                const dateTimeString = `${showDate}T${time}`;
+                showsToCreate.push({
+                    movie : movieId,
+                    showDateTime : new Date(dateTimeString),
+                    showPrice,
+                    occupiedSeats : {}
+                })
+            })
+        });
+
+        if(showsToCreate.length > 0)
+        {
+            await Show.insertMany(showsToCreate);
+        }
+        res.json({success : true, message : 'Show Added successfully.'})
+    }catch (error){
+        console.error(error)
+        res.json({success : false, message : error.message})
+    }
+}
+
+//API to get all shows from the database
+export const getShows = async (req,res) =>{
+    try {
+        const shows = await Show.find({showDataTime : {$gte : new Date()}}).populate('movie').sort({showDataTime : 1});
+
+        //filter unique shows
+        const uniqueShows = new Set(shows.map(show => show.movie))
+
+        res.json({success : true, show : Array.from(uniqueShows)})
+    } catch (error){
+        console.error(error);
+        res.json({success : false, message : error.message})
+    }
+}
+
+//API to get a single show from the database
+export const getShow = async (req,res) =>{
+    try {
+        const {movieId} = req.params;
+        //get all upcoming shows for the movie
+        const shows = await Show.find({movie : movieId, showDataTime : {$gte: new Date()}}) 
+
+        const movie = await Movie.findById(movieId);
+        const dateTime = {};
+
+        shows.forEach((show) => {
+            const date = show.showDataTime.toISOString().split("T")[0];
+            if  (!dateTime[date]){
+                dateTime[data] = []
+            }
+            dateTime[date].push({time : show.showDataTime, showId : show._id})
+        })
+        res.json({success : true, movie,dateTime})
+
+    } catch (error) {
+        console.error(error);
+        res.json({success : false, message : error.message})
+    }
+}
--- server/server.js
+++ server/server.js
@@ -5,6 +5,9 @@ import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
+import showRouter from './routes/showRoutes.js';
+import bookingRouter from './routes/bookingRoutes.js';
+import adminRouter from './routes/adminRoutes.js';
 
 
 const app = express()
@@ -21,5 +24,8 @@ app.use(clerkMiddleware())
 //API Routes
 app.get('/', (req,res)=>res.send('Server is Live!'))
 app.use('/api/inngest', serve({ client: inngest, functions }))
+app.use('/api/show', showRouter)
+app.use('/api/booking', bookingRouter)
+app.use('/api/admin', adminRouter)
 
 app.listen(port, ()=> console.log(`server listening at http://localhost:${port}`))
